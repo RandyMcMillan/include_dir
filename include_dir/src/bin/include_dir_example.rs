e7a7fba9b8cdd36cf6fcb85e660fffb39307a1d7
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-03 15:21:31 -0400

    include_dir/src/bin/include_dir_example.rs:intermediate
    print!("{:?}",Some(lib_rs).unwrap());

--- include_dir/src/bin/include_dir_example.rs
+++ include_dir/src/bin/include_dir_example.rs
@@ -13,5 +13,13 @@ fn main() {
     #[cfg(feature = "glob")]
     for entry in PROJECT_DIR.find(&glob).unwrap() {
         println!("Found {}", entry.path().display());
+        let lib_rs = PROJECT_DIR.get_file(format!("{}",entry.path().display()));
+        print!("{:?}",Some(lib_rs).unwrap());
+        //let body = Some(lib_rs.expect("REASON").contents_utf8());
+        //let body = lib_rs.contents_utf8().unwrap();
+        //let body = lib_rs.expect("REASON").contents_utf8().unwrap();
+        //print!("{:?}",Some(body).unwrap());
+        //let entry_body: &str = PROJECT_DIR.get_file(entry.path().display().unwrap());
+        //let body = entry.contents_utf8().unwrap();
     }
 }
