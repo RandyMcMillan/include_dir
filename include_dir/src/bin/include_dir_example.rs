7435726fab669bcf9fc51ba40607cf1a0e9f1a0e
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-03 16:09:42 -0400

    include_dir/src/bin/include_dir_example.rs:Some(Some...

--- include_dir/src/bin/include_dir_example.rs
+++ include_dir/src/bin/include_dir_example.rs
@@ -13,10 +13,26 @@ fn main() {
     #[cfg(feature = "glob")]
     for entry in PROJECT_DIR.find(&glob).unwrap() {
         println!("Found {}", entry.path().display());
-        let lib_rs = PROJECT_DIR.get_file(format!("{}",entry.path().display()));
-        print!("{:?}\n",Some(lib_rs).unwrap());
+        print!(
+            "16:{:?}\n",
+            PROJECT_DIR.get_file(format!("{}", entry.path().display()))
+        );
+        let file = PROJECT_DIR.get_file(format!("{}", entry.path().display()));
+        print!("{:?}\n", Some(file).unwrap());
         //let body = Some(lib_rs.expect("REASON").contents_utf8());
-        //let body = lib_rs.contents_utf8().unwrap();
+
+        if file.is_none() {
+            print!("file.is_none()={}\n", file.is_none());
+        } else {
+            //let file_contents = Some(file.expect("REASON").contents_utf8().unwrap());
+            let mut file_contents = Some(file.expect("REASON").contents_utf8());
+            if file_contents.is_none() {
+                print!("file_contents.is_none()={}\n", file_contents.is_none());
+            } else {
+                print!("file_contents.is_none()={}\n", file_contents.is_none());
+		file_contents = Some(Some(lib_rs.contents_utf8().unwrap()));
+            }
+        }
         //let body = lib_rs.expect("REASON").contents_utf8().unwrap();
         //print!("{:?}",Some(body).unwrap());
         //let entry_body: &str = PROJECT_DIR.get_file(entry.path().display().unwrap());
