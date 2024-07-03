4e1cd80dd81e160f1ed3769bbd341b59b8a5eaff
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-03 14:40:45 -0400

    include_dir/src/bin/include_dir_example.rs:intermediate:
    [cfg(feature = "glob")]
    [cfg(feature = "metadata")]

--- include_dir/src/bin/include_dir_example.rs
+++ include_dir/src/bin/include_dir_example.rs
@@ -6,9 +6,9 @@ fn main() {
     let lib_rs = PROJECT_DIR.get_file("src/lib.rs").unwrap();
     let body = lib_rs.contents_utf8().unwrap();
     assert!(body.contains("globs"));
-    #[cfg(feature = "glob")]
-    #[cfg(feature = "metadata")]
+    //#[cfg(feature = "metadata")]
     let glob: &str = "**/*.rs";
+    #[cfg(feature = "glob")]
     for entry in PROJECT_DIR.find(&glob).unwrap() {
         println!("Found {}", entry.path().display());
     }
