e05797ab1155a0bb1a5f5d3fbd4cc696601f09b7
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-03 18:00:11 -0400

    include_dir/src/bin/include_dir_example.rs:clean up code

--- include_dir/src/bin/include_dir_example.rs
+++ include_dir/src/bin/include_dir_example.rs
@@ -10,18 +10,15 @@ fn main() {
     let glob: &str = "**";
     #[cfg(feature = "glob")]
     for entry in PROJECT_DIR.find(&glob).unwrap() {
-
         let file = PROJECT_DIR.get_file(format!("{}", entry.path().display()));
         if file.is_none() {
         } else {
-            let mut file_contents = file.expect("REASON").contents_utf8();
-            let mut file_path = file.expect("REASON").path();
+            let file_contents = file.expect("REASON").contents_utf8();
+            let file_path = file.expect("REASON").path();
             if file_contents.is_none() {
-                //print!("file_contents.is_none()={}\n", file_contents.is_none());
             } else {
-                //print!("file_contents.is_none()={}\n", file_contents.is_none());
-                print!("{:}\n",file_path.display());
-                print!("{:}",file_contents.clone().unwrap());
+                print!("{:}\n", file_path.display());
+                print!("{:}", file_contents.clone().unwrap());
             }
         }
     }
