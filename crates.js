6587a27be50fdbf481b0d1bfb94f1d52df1eaa15
RandyMcMillan <randy.lee.mcmillan@gmail.com>
2024-07-03 23:54:07 +0000

    Deploying to gh-pages from @ gnostr-org/include_dir@8f3435f83d6b3a87a583049e50a02ac4fc96cb5c 🚀

--- crates.js
+++ crates.js
@@ -1,1 +1,1 @@
-window.ALL_CRATES = ["glob","include_dir","include_dir_example","include_dir_macros","proc_macro2","quote","unicode_ident"];
+window.ALL_CRATES = ["block_buffer","cfg_if","cpufeatures","crypto_common","digest","generic_array","glob","hex_literal","include_dir","include_dir_example","include_dir_macros","proc_macro2","quote","sha2","typenum","unicode_ident"];
