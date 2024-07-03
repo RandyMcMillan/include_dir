8a51fa3547069def7f186f78655ca9a608f800eb
randymcmillan <randy.lee.mcmillan@gmail.com>
2024-07-03 14:27:30 -0400

    include_dir_example:output clean

---
+++ include_dir/src/bin/include_dir_example.rs
@@ -0,0 +1,28 @@
+use include_dir::{include_dir, Dir};
+
+static PROJECT_DIR: Dir = include_dir!("$CARGO_MANIFEST_DIR");
+
+fn main() {
+    let lib_rs = PROJECT_DIR.get_file("src/lib.rs").unwrap();
+    let body = lib_rs.contents_utf8().unwrap();
+    assert!(body.contains("globs"));
+    #[allow(unused_variables)]
+    let glob: &str = "**";
+    #[cfg(feature = "glob")]
+    for entry in PROJECT_DIR.find(&glob).unwrap() {
+
+        let file = PROJECT_DIR.get_file(format!("{}", entry.path().display()));
+        if file.is_none() {
+        } else {
+            let mut file_contents = file.expect("REASON").contents_utf8();
+            let mut file_path = file.expect("REASON").path();
+            if file_contents.is_none() {
+                //print!("file_contents.is_none()={}\n", file_contents.is_none());
+            } else {
+                //print!("file_contents.is_none()={}\n", file_contents.is_none());
+                print!("{:}\n",file_path.display());
+                print!("{:}",file_contents.clone().unwrap());
+            }
+        }
+    }
+}
